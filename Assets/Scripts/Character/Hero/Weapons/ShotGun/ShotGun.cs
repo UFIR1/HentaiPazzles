ac17be02717b5c998954492a5ef2a144eee58946
UFIR1 <48480127+UFIR1@users.noreply.github.com>
2021-07-10 19:12:36 +0300

    Добавлена модульность к оружию

--- Assets/Scripts/Character/Hero/Weapons/ShotGun/ShotGun.cs
+++ Assets/Scripts/Character/Hero/Weapons/ShotGun/ShotGun.cs
@@ -5,45 +5,26 @@ using UnityEngine;
 
 public class ShotGun : BaseWeapon
 {
-	[SerializeField]
-	private int bulletCountPerShot =5;
-	[SerializeField]
-	private float spread = 0;
+    public OneShotFireModule startedFireModeModule;
+    public MultiShootingModule startedShootingModule;
 
+    public override BaseFireModeModule StartedFireModeModule { get => startedFireModeModule; }
+    public override BaseShootingModule StartedShootingModule { get => startedShootingModule;  }
 
-	public override void TriggerDown(BaseHero Sander)
-	{
-		if (CurrentMagazineLoaded > 0)
-		{
-			CurrentMagazineLoaded -= 1;
-			var newBullets = new List<GameObject>();
-			for (int i = 0; i < bulletCountPerShot; i++)
-			{
-				var bulletRotation = new Quaternion(
-					bulletSpawnerPoint.transform.rotation.x,
-					bulletSpawnerPoint.transform.rotation.y,
-					bulletSpawnerPoint.transform.rotation.z + Random.Range(-spread, spread),
-					bulletSpawnerPoint.transform.rotation.w
-					);
-				var newBulletObj = Instantiate(CurrentBullet.bullet.gameObject, bulletSpawnerPoint.transform.position, bulletRotation);
-				newBulletObj.SetActive(false);
-				var newBullet = newBulletObj.GetComponent<BaseBullet>();
-				newBullet.Shooter = Sander;
-				var module = passModules.Where(x => x.bulletType == newBullet.GetType()).FirstOrDefault();
-				module.PullBullet(ref newBullet);
-				newBullets.Add(newBulletObj);
-			}
-			foreach (var item in newBullets)
-			{
-				item.SetActive(true);
-			}
-		}
-			
-		
-	
-	}
+    protected override void LocalStart()
+    {
+        
+    }
 
-	public override void TriggerUp(BaseHero Sander)
-	{
-	}
+    public override void TriggerDown(BaseHero Sander)
+    {
+        base.TriggerDown(Sander);
+    }
+
+    public override void TriggerUp(BaseHero Sander)
+    {
+        base.TriggerUp(Sander);
+    }
+
+    
 }
---
+++ Assets/Scripts/Character/Hero/Weapons/WeponModules/FireMods/OneShotFireModule.cs
@@ -0,0 +1,13 @@
+﻿[System.Serializable]
+public class OneShotFireModule : BaseFireModeModule
+{
+    public override void TriggerDown(BaseHero Sander,BaseShootingModule shootingModule, BaseWeapon weapon)
+    {
+        shootingModule.Shot(Sander,weapon);
+    }
+
+    public override void TriggerUp(BaseHero Sander)
+    {
+        
+    }
+}
